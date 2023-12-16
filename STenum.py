292f4954995bb706ba37bd8ebbbaf223b2fcad28
MaakResearch <121325637+MaakResearch@users.noreply.github.com>
2023-12-16 16:18:15 -0500

    Update and rename securitytrailsSubdomains.py to STenum.py

--- STenum.py
+++ STenum.py
@@ -1,19 +1,14 @@
+import argparse
 import requests
 import urllib3
 import json
-import sys
-
-if len(sys.argv) != 2:
-	print(f"\n[+] Usage: python {sys.argv[0]} domain\n\nEx: python {sys.argv[0]} apple.com")
-	exit()
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-domain = sys.argv[1]
 # Your API KEY here, Visit https://securitytrails.com/app/account/credentials to get it.
-APIKEY = "1KikdQkjao35iC2zP1DtcKGl60PzQmsA" 
+APIKEY = "PUT YOUR OWN API KEY HERE" 
 
-def collect(domain):
+def collect(domain,exportfile):
 	subdomainURL = f"https://api.securitytrails.com/v1/domain/{domain}/subdomains"
 	headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:112.0) Gecko/20100101 Firefox/112.0","apikey":APIKEY}
 	response = requests.get(subdomainURL, headers=headers, verify=False)
@@ -23,14 +18,26 @@ def collect(domain):
 	# Save result to file in current dir called securitytrails_subdomains.txt
 	subdomainLength = len(resArray['subdomains'])
 	for num in range(subdomainLength):
-		with open("securitytrails_subdomains.txt","a") as f:
+		with open(exportfile,"a") as f:
 			subdomain = str(resArray['subdomains'][num]+"."+domain)
 			f.write(subdomain)
 			f.write("\n")
 	f.close()
-	print(f"[+] Successfully Subdomains Collected {subdomainLength} from SecurityTrails in file securitytrails_subdomains.txt")
+	print(f"[+] Successfully Subdomains Collected {subdomainLength} from SecurityTrails in file {exportfile}")
 
 if __name__ == "__main__":
-	collect(domain)
+	parser = argparse.ArgumentParser(description='SecurityTrails Subdomain Enum tool.')
 
+	parser.add_argument('-d', '--domain',required=True, help='The domain name ex:apple.com')
+	parser.add_argument('-f', '--file', required=True, help='The output file name ex:subdomain.txt')
+
+	args = parser.parse_args()
+
+	domain = str(args.domain)
+	exportfile = str(args.file)
+	try:
+
+		collect(domain,exportfile)
+	except Exception as err:
+		print(err)
 
